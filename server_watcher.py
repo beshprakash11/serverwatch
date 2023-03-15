9ab3c12363451cd192fe83d03ad76efc1bc7b65a
beshprakash11 <beshprakashktm@gmail.com>
2023-03-15 10:34:08 +0100

    Watch directory, source, and destnation directory

--- server_watcher.py
+++ server_watcher.py
@@ -3,3 +3,13 @@ import os
 import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
+"""
+Watch directory, source, and destnation directory
+"""
+watch_dir = "./watch_dir/src/"
+src_1 = "./watch_dir/src/"
+src_2 = "./watch_dir/src/"
+src_dst = "./watch_dir/dst/"
+
