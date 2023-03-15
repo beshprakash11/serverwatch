ba7fb968870f909de0ab8cda4d5bab4e810d30c0
beshprakash11 <beshprakashktm@gmail.com>
2023-03-15 12:49:18 +0100

    watcher created in directory watcher

--- watach_dir.py
+++ watach_dir.py
@@ -3,3 +3,22 @@ import os
 import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+    DIRECTORY_TO_WATCH = "./watch_dir/src/"
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print ("Error")
+
+        self.observer.join()
