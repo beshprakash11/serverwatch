75f1430a2d7a559e88d5f4ecb9e4243ffa4fb25a
beshprakash11 <beshprakashktm@gmail.com>
2023-03-15 10:36:11 +0100

    watcher class added with run and init

--- server_watcher.py
+++ server_watcher.py
@@ -13,3 +13,20 @@ src_1 = "./watch_dir/src/"
 src_2 = "./watch_dir/src/"
 src_dst = "./watch_dir/dst/"
 
+
+class Watcher:
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print ("Error")
+
+        self.observer.join()
